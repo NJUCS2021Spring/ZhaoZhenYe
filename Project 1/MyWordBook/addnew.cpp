6c09bf89e6ff7071f046fd3ace734da541a5540b
Cardinal2002 <201220192@smail.nju.edu.cn>
2021-02-21 23:13:07 +0800

    promote the basic function of adding groups and optimize the page

--- Project 1/MyWordBook/addnew.cpp
+++ Project 1/MyWordBook/addnew.cpp
@@ -10,11 +10,25 @@ struct word
 };
 void addnew(int group_num)
 {
-    word new_word;
-    cin >> new_word.content >> new_word.meaning;
     char F_PATH[80];
+    char reason[20];
     sprintf_s(F_PATH, "D:\\mycode\\Project 1\\%d.txt", group_num);
     ofstream out(F_PATH);//fp = fopen(F_PATH, "w");  //创建文件
-    out << new_word.content << " " << new_word.meaning;
+    cout << "------------------------------------------------------------------------------"<<endl;//78
+    cout << "分组ID: " << group_num<<endl;
+    cout << "请输入分组依据：";
+    cin >> reason;
+    out << reason << endl;
+    for(int i=1;;i++)
+    {
+    cout << "请输入组内单词：";
+    word new_word;
+    cin >> new_word.content;
+    out << new_word.content << endl;
+    cout << "是否继续输入单词？(y/n)" << endl;
+    char anser;
+    cin >> anser;
+    if (anser == 'n') break;
+    }
     out.close();
 }
--- Project 1/MyWordBook/addnew.h
+++ Project 1/MyWordBook/addnew.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef __ADDNEW_H__
 #define __ADDNEW_H__
-
+struct word;
 void addnew(int group_num);
 
 #endif;
